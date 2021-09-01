752919af722532755dce1e0c0d63b802b8dcae02
Parth Thosani <43791395+thosaniparth@users.noreply.github.com>
2021-09-01 16:24:15 -0400

    Update init_test.py

--- test/init_test.py
+++ test/init_test.py
@@ -1,1 +1,8 @@
+def test_odd_even():
+    assert even_odd(7) == "number is odd", "Should be odd"
+    assert even_odd(0) == "number is even", "Should be even"
+    
 
+if __name__ == "__main__":
+    test_odd_even()
+    print("2/2 test cases passed")
