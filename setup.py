4ce99363708c6916267447e3428e815dec73185f
nehaAgarwal96 <89555333+nehaAgarwal96@users.noreply.github.com>
2021-09-01 20:00:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,16 @@ setup(
     description='Software Engineering Homework 2b',
     author='Neha Agarwal',
     author_email='nehaagarwal121@gmail.com',     
+    url="https://github.com/Software-Engineering-Group-15-Fall-2021/HW-2b",
+     long_description="""\
+       It computes whether a number is odd or even.
+      """,
     license="MIT",
     classifiers=[
-          "License :: MIT",
+          "License :: OSI Approved :: MIT",
           "Programming Language :: Python",       
           "Intended Audience :: Developers",
           "Topic :: Programming example",
       ],      
-      install_requires=['numpy','matplotlib','pandas']
+      install_requires=['numpy','matplotlib','pandas'],
       )
